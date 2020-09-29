e6747539818bbe947a8fdc4a9bdee09aa9184f9f
Amir <amir@musaev.online>
2020-09-29 22:28:29 +0000

    feat: Add models

--- api/models.py
+++ api/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Chat(models.Model):
+    name = models.CharField(max_length=255)
+
+
+class Message(models.Model):
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE)
+    author = models.IntegerField()
+    text = models.TextField()
+
+
+class UserCounter(models.Model):
+    pass
