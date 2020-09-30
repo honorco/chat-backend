bd62d9f8e4cf368ef695394fb53cf1d4df7c54e6
Amir <amir@musaev.online>
2020-09-30 09:03:49 +0000

    feat: Add ID generator

--- api/urls.py
+++ api/urls.py
@@ -2,5 +2,6 @@ from django.contrib import admin
 from django.urls import path, include
 from . import  views
 urlpatterns = [
-    path('', views.index, name="index")
+    path('', views.index, name="index"),
+    path('id/', views.getID, name="id")
 ]
--- api/views.py
+++ api/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from api.models import UserCounter
 
 def index(req):
     return HttpResponse('Hello, world!')
+def getID(req):
+    user = UserCounter()
+    user.save()
+    return HttpResponse(user.id)
