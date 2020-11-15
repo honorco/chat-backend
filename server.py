b91aa735abce87673681cd9301b049b5bc9eba21
MaksimKanaev <Kanaev24.7@yandex.ru>
2020-11-15 12:50:47 +0300

    Добавление callbacks

--- server.py
+++ server.py
@@ -1,4 +1,7 @@
 from abc import ABC
+from typing import List, Tuple, Dict
+from collections.abc import Callable
+
 try:
     import thread
 except ImportError:
@@ -9,6 +12,8 @@ import tornado.ioloop
 import json
 import asyncio
 import psycopg2
+import datetime
+import time
 
 con = psycopg2.connect(
     database="db_chat",
@@ -18,26 +23,34 @@ con = psycopg2.connect(
     port="5432"
 )
 
-cur = con.cursor()
-
-
 clients = []
+callbacks: Dict[int, Callable] = {}
+
+
+def myconverter(o):
+    if isinstance(o, datetime.datetime):
+        return "{}-{}-{}".format(o.year, o.month, o.day)
 
 
 class MessageController:
     @staticmethod
     def create(connection, data):
         data = json.loads(data)
-        print(data)
+        cur = con.cursor()
         cur.execute(
             f"INSERT INTO message (text_message, time, author, chat_id) VALUES ('{data['text_message']}', '{data['time']}', '{data['author']}', '{data['chat_id']}')"
         )
         con.commit()
+        con.close()
         for client in [c for c in clients if c != connection]:
             client.send('/messages/create', data)
 
     @staticmethod
-    def get():
+    def get(connection, data):
+        return "this is data"
+
+    @staticmethod
+    def get_list(connection, data):
         pass
 
 
@@ -47,8 +60,7 @@ class ChatController:
         pass
 
 
-routes = {"/messages/create": MessageController.create, "/chats/get": ChatController.get,
-          "/messages/get": MessageController.get}
+routes = {"/messages/create": MessageController.create, "/messages/get": MessageController.get, "/chats/get": ChatController.get, "/messages/get_list": MessageController.get_list}
 
 
 class ServerConnector(tornado.websocket.WebSocketHandler, ABC):
@@ -56,14 +68,32 @@ class ServerConnector(tornado.websocket.WebSocketHandler, ABC):
         clients.append(self)
         print("WebSocket opened")
 
-    def on_message(self, message):
+    def on_message(self, message: str):
         print(message)
-        message = json.loads(message)
-        if message['url'] and message['data'] and routes[message['url']]:
-            routes[message['url']](self, message['data'])
+        message: Dict = json.loads(message)
+        server_id = message.get('server_id')
+        client_id = message.get('client_id')
+        url = message.get('url')
+        if url and client_id:
+            handler = routes.get(url)
+            res = handler(self, message.get('data')) or None
+            self.write_message(json.dumps({"client_id": client_id, "data": res}))
+        elif url:
+            handler = routes.get(url)
+            handler(self, message.get('data'))
+        elif server_id:
+            handler = callbacks.pop(server_id)
+            handler(self, message.get('data'))
 
-    def send(self, url, msg):
-        self.write_message(json.dumps({"url": url, "data": msg}))
+    def send(self, url, message=None, callback=None):
+        res = {"url": url}
+        if message:
+            res["data"] = message
+        if callback:
+            server_id = time.time_ns()
+            res['server_id'] = server_id
+            callbacks[server_id] = callback
+        self.write_message(json.dumps(res))
 
     def on_close(self):
         clients.remove(self)
@@ -84,4 +114,5 @@ def initServer():
 thread.start_new_thread(initServer, ())
 
 while True:
-    pass
+    msg = input()
+    clients[0].send('check', callback=lambda self, x: print(x))
